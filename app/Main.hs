58faa1dd659dfb7097558ab0b93ad3ca31671c8e
Otto Martikainen <martikainenotto@gmail.com>
2018-06-25 20:54:16 +0300

    Euler 3 solved

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Lib
 results :: [String]
 results = [ euler1
           , euler2
+          , euler3
           ]
 
 main :: IO ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,11 @@
 module Lib
   ( euler1
   , euler2
+  , euler3
   ) where
 
+import Data.List (sort, find)
+import Data.Maybe (fromMaybe, isJust)
 
 -- https://projecteuler.net/problem=1
 euler1 :: String
@@ -10,8 +13,28 @@ euler1 = show result where
   result = sum [ x | x <- [0..999],
                      any (==0) [ x `mod` 3, x `mod` 5 ] ]
 
+
 -- https://projecteuler.net/problem=2
+fibs :: [Int]
+fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
+
 euler2 :: String
 euler2 = show result where
   result = sum $ filter even $ takeWhile (<4000000) fibs
-  fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
+
+
+-- https://projecteuler.net/problem=3
+primes :: [Int]
+primes = filterPrime [2..] where
+  filterPrime (p:xs) = p : filterPrime [x | x <- xs, x `mod` p /= 0]
+
+factorsOf :: Int -> [Int]
+factorsOf x = factors where
+  factor = head $ filter (\p -> x `mod` p == 0) primes
+  factors = case (x `div` factor) of
+    1   -> [factor]
+    rem -> factor : factorsOf rem
+
+euler3 :: String
+euler3 = show result where
+  result = head $ reverse $ factorsOf 600851475143
